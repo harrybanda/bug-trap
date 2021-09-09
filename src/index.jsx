c09bba94f4d9c5a4e8bbff649a3cb6fc47c426fb
Harry Banda <harry.banda@mudaall.com>
2021-09-09 23:41:11 +0200

    Added setup instructions

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import ForgeUI, {
   Fragment,
   Macro,
   Text,
+  Strong,
   useEffect,
   useState,
   Code,
@@ -33,12 +34,48 @@ const WebTriggerUrl = () => {
 
 const App = () => {
   const [isOpen, setOpen] = useState(false);
+
+  const installCode = `# using yarn
+yarn add react-bug-trap
+# using npm
+npm install react-bug-trap`;
+
+  const configCode = `// import the module
+import { BugTrap } from "react-bug-trap";
+// wrap any component with <BugTrap>
+<BugTrap webhook="YOUR_FORGE_WEBHOOK">
+  <App />
+</BugTrap>`;
+
   return (
     <Fragment>
-      <Button text="Show modal" onClick={() => setOpen(true)} />
+      <Button text="React Application Setup" onClick={() => setOpen(true)} />
       {isOpen && (
-        <ModalDialog header="My modal dialog" onClose={() => setOpen(false)}>
-          <Text>Hello there!</Text>
+        <ModalDialog
+          header="React Application Setup"
+          onClose={() => setOpen(false)}
+        >
+          <Text>
+            <Strong>Installation</Strong>
+          </Text>
+          <Text>
+            This module should be installed as one of your project's
+            dependencies:
+          </Text>
+          <Code text={installCode} language="shell" />
+          <Text>
+            <Strong>Usage</Strong>
+          </Text>
+          <Text>
+            Wrap <Code text="<BugTrap>" language="javascript" /> around any
+            component that may throw an error. This will handle errors thrown by
+            that component and its descendants too.
+          </Text>
+          <Code text={configCode} language="javascript" />
+          <Text>
+            Replace <Code text="YOUR_FORGE_WEBHOOK" language="javascript" />{" "}
+            with the URL below:
+          </Text>
           <WebTriggerUrl />
         </ModalDialog>
       )}
